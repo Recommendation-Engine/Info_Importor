0bbf61f4f74132b277e6f8ed626eb42a39fa7fa2
Andy Zeng <azeng@thoughtworks.com>
2015-04-30 12:19:37 +1000

    Added resultSplitter.py

---
+++ importer/resultSplitter.py
@@ -0,0 +1,20 @@
+def main():
+	inputFile = 'result.txt'
+	errorFile = 'errorfile.txt'
+	successFile = 'successfile.txt'
+	with open(inputFile, 'r') as inFile:
+		with open(successFile, 'w') as suFile:
+			with open(errorFile, 'w') as erFile:
+				lines = inFile.readlines()
+				for i in range(len(lines)):
+					if i % 3 == 0:
+						title_line = lines[i]
+						result_line = lines[i+2]
+						if result_line.strip() == '{"Response":"False","Error":"Movie not found!"}':
+							erFile.write(title_line+'')
+						else:
+							suFile.write(title_line+'')
+							suFile.write(result_line+'\n')
+
+if __name__ == '__main__':
+	main()
